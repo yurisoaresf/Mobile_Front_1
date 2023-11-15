836025da963253a21c6a8565c0410d729e59dd74
Lucas Porto <teste123124@gmail.com>
2023-11-14 21:24:56 -0300

    hot fix createAccount root

--- src/pages/CreateAccount/CreateAccount.tsx
+++ src/pages/CreateAccount/CreateAccount.tsx
@@ -3,17 +3,15 @@ import { Text, TextInput, Button, View, ScrollView} from 'react-native'
 import styles from './CreateAccountStyle';
 import { Card } from 'react-native-elements';
 import Button2 from '../Components/Button';
+import { useFonts } from 'expo-font';
 import { useCallback } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
-import { useFonts } from 'expo-font';
-
 
 
 const CreateAccount = ({navigation}: any) => {
   const goToPage = (path: any) => {
     navigation.navigate(path);
-
-
+  }
     const [fontsLoaded,fontError] = useFonts({
       'Benguiat': require('../../../assets/fonts/Benguiat.ttf'),
       'Aachen': require('../../../assets/fonts/Aachen.ttf'),
@@ -29,10 +27,9 @@ const CreateAccount = ({navigation}: any) => {
       return null;
     }
 
-}
   return (
 
-      <View style={styles.container}>
+      <View style={styles.container} onLayout={onLayoutRootView}>
           <Text style={[styles.setTextColor, {fontFamily:'Benguiat'}]}>Nome:</Text>
           <TextInput style={[styles.input, {fontFamily:'Benguiat'}]}/>
           <Text style={[styles.setTextColor, {fontFamily:'Benguiat'}]}>Email:</Text>
@@ -41,7 +38,6 @@ const CreateAccount = ({navigation}: any) => {
           <TextInput style={[styles.input, {fontFamily:'Benguiat'}]}/>
           <Text style={[styles.setTextColor, {fontFamily:'Benguiat'}]}>Confirmar Senha:</Text>
           <TextInput style={[styles.input, {fontFamily:'Benguiat'}]}/>
-
           <Button2 onPress={()=> (goToPage("Login"))} labelButton="Criar Conta"></Button2>
       </View>
   )
